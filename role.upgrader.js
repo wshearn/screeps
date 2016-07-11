4111f4048f420f2dcd4decde9ea907c87fca4c2d
Wesley Hearn <whearn@redhat.com>
2016-07-10 21:10:43 -0400

    Fix tabs, supposed to be spaces like a sane person

--- role.upgrader.js
+++ role.upgrader.js
@@ -15,12 +15,12 @@ class Upgrader extends _super {
         }
         if(creep.memory.upgrading && creep.carry.energy == 0) {
             creep.memory.upgrading = false;
-	    }
-	    if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
-	        creep.memory.upgrading = true;
-	    }
+        }
+        if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.upgrading = true;
+        }
 
-	    if(creep.memory.upgrading) {
+        if(creep.memory.upgrading) {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller);
             }
